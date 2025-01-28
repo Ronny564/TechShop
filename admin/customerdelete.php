53166c5b34aa21708da86390fad220911bed4893
Johnny <linlin999.ygn@gmail.com>
2025-01-28 13:10:52 +0630

    final commit

--- admin/customerdelete.php
+++ admin/customerdelete.php
@@ -3,9 +3,24 @@ require_once("../database/PDO.php");
 
 $id = $_POST['id'];
 
-$sql = "DELETE FROM  customers WHERE CusId=:id";
+$sql1 = "DELETE FROM saledetail WHERE CusID = :id";
+$stmt1 = $pdo->prepare($sql1);
+$stmt1->bindParam(":id", $id);
+$stmt1->execute();
+
+$sql2 = "DELETE FROM sale WHERE CusId = :id";
+$stmt2 = $pdo->prepare($sql2);
+$stmt2->bindParam(":id", $id);
+$stmt2->execute();
+
+$sql3 = "DELETE FROM wishlist WHERE CusId = :id";
+$stmt3 = $pdo->prepare($sql3);
+$stmt3->bindParam(":id", $id);
+$stmt3->execute();
+
+$sql = "DELETE FROM customers WHERE CusId = :id";
 $stmt = $pdo->prepare($sql);
-$stmt->bindParam(":id",  $id);
+$stmt->bindParam(":id", $id);
 $stmt->execute();
 
 header("Location: customer.php");
